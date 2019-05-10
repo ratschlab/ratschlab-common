45636d5ebcd94cc7e01ae186ba1e03149ad2edc1
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2019-05-10 15:40:55 +0200

    regorganizing spark setting up

--- ratschlab_common/utils.py
+++ ratschlab_common/utils.py
@@ -1,25 +1,43 @@
 from typing import Any, Sequence
 from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
 from distributed import LocalCluster
 from pathlib import Path
 import tempfile
 
 
-def create_spark_session(cores: int, memory_per_executor: int):
-    driver_mem = cores * memory_per_executor + 2000 # ok? parametrize?
+def default_spark_config(cores: int, memory_per_executor: int, driver_overhead: int = 2000, tmp_dir: str = '', extra_java_options: str = '', enable_arrow: bool = True) -> SparkConf:
+    driver_mem = cores * memory_per_executor + driver_overhead
 
     jars = [str(p) for p in Path(Path(__file__).parent.parent, "jars").glob(
         '*.jar')]
     jar_paths = ':'.join(jars)
 
-    return(SparkSession.
+    cfg = SparkConf()
+
+    if tmp_dir:
+        cfg.set("spark.local.dir", tmp_dir)
+
+    # avoiding trouble with JDBC and timestamps
+    java_options = "-Duser.timezone=UTC " + str(extra_java_options)
+
+    return (cfg.set("spark.driver.memory", "{}m".format(driver_mem)).
+            set("spark.executor.memory", "{}m".format(memory_per_executor)).
+            set("spark.driver.extraJavaOptions", java_options).
+            set("spark.master", "local[{}]".format(cores)).
+            set("spark.jars", jar_paths).
+            set("spark.sql.execution.arrow.enabled", str(enable_arrow))
+            )
+
+
+def create_spark_session(cores: int, memory_per_executor: int) -> SparkSession:
+    return create_spark_session_from_config(default_spark_config(cores, memory_per_executor))
+
+
+def create_spark_session_from_config(cfg: SparkConf) -> SparkSession:
+    return (SparkSession.
              builder.
-             config("spark.driver.memory", "{}m".format(driver_mem)).
-             # avoiding trouble with JDBC and timestamps
-             config("spark.driver.extraJavaOptions", "-Duser.timezone=UTC").
-             config("spark.jars", jar_paths).
-             config("spark.executor.memory", "{}m".format(memory_per_executor)).
-             config("spark.master", "local[{}]".format(cores)).
+             config(conf=cfg).
              getOrCreate())
 
 
