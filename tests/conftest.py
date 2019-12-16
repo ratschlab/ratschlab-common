7a44fe5f5998636fb8549bdf2d0d0541823461ef
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2019-12-16 13:29:54 +0100

    refactoring fixtures, writing tests for export_db_to_files, fixing issue with context handling in export_db_to_files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,3 @@
-import pytest
-import pandas as pd
-from faker import Factory
-import random
+from tests.db.data_fixtures import *
 
-# TODO refactor this out?
-@pytest.fixture(scope='session')
-def a_df():
-    fake = Factory.create()
-    fake.seed_instance(420)
 
-    nrows = 100
-    records = []
-    for _ in range(nrows):
-        records.append([
-            fake.name(),
-            fake.email(),
-            fake.date_time(),
-            random.random(),
-            random.randint(1, 20)
-        ])
-
-    col_names = ["col{}".format(i) for i in range(len(records[0]))]
-    return pd.DataFrame.from_records(records, columns=col_names)
