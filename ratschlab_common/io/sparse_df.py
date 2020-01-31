5155b878750c78e38dca6d4f7d7e98d94536c874
lorenzonoci <lore.noci@gmail.com>
2020-01-31 12:44:52 +0100

    handling optional arguments

--- ratschlab_common/io/sparse_df.py
+++ ratschlab_common/io/sparse_df.py
@@ -13,14 +13,14 @@ class SparseDFReader:
         self.path = path
 
     def read_hdf(self):
-        self.f = tables.open_file(self.path, mode='r')
-        sparse_group = self.f.get_node("/data/sparse")
+        f = tables.open_file(self.path, mode='r')
+        sparse_group = f.get_node("/data/sparse")
         sparse_m = self._read_sparse_m(sparse_group)
-        s_col_names = self.f.get_node("/data/sparse/col_names")
+        s_col_names = f.get_node("/data/sparse/col_names")
         non_sparse = pd.read_hdf(self.path, key="/data/non_sparse")
         sparse = pd.DataFrame.sparse.from_spmatrix(sparse_m, columns=s_col_names)
-        ret_df = pd.concat([sparse, non_sparse], axis=1) 
-        self.f.close()
+        ret_df = pd.concat([sparse, non_sparse], axis=1)
+        f.close()
         return ret_df
 
     @staticmethod
@@ -33,7 +33,11 @@ class SparseDFReader:
 
 class SparseDFWriter:
 
-    def __init__(self, df):
+    def __init__(self, df, complevel=5, complib='blosc'):
+
+        # Optional arguments
+        self.complevel = complevel
+        self.complib = complib
 
         # from nan to zeros (pandas sparse array wants nan by default, scipy sparse matrix zeros)
         # self.df = df.fillna(0.0)
@@ -46,30 +50,36 @@ class SparseDFWriter:
         self.sparse_m = sparse.csr_matrix(self.sparse.values)
 
     @staticmethod
-    def _store_sparse_m(m, hdf5, group):
+    def _store_sparse_m(m, hdf5, group, complevel=5, complib='blosc', **kwargs):
         assert (m.__class__ == sparse.csr.csr_matrix), 'M must be a csr matrix'
         for attribute in ('data', 'indices', 'indptr', 'shape'):
             arr = np.array(getattr(m, attribute))
-            SparseDFWriter._store_array_hdf(arr, hdf5, group, attribute)
+            SparseDFWriter._store_array_hdf(arr, hdf5, group, attribute,
+                                            complevel=complevel, complib=complib, **kwargs)
         return hdf5
 
     @staticmethod
-    def _store_array_hdf(arr, hdf5, group, name):
+    def _store_array_hdf(arr, hdf5, group, name, complevel=5, complib='blosc', **kwargs):
         if arr.dtype == 'object':
             arr = arr.astype('U')
         atom = tables.Atom.from_dtype(arr.dtype)
-        filters = tables.Filters(complevel=9, complib='blosc')
+        filters = tables.Filters(complevel=complevel, complib=complib, **kwargs)
         ds = hdf5.create_carray(group, name, atom, arr.shape, filters=filters)
         ds[:] = arr
 
 
-    def to_hdf(self, path):
+    def to_hdf(self, path, **kwargs):
+        kwargs_sparse = kwargs.get('sparse', {})
+        kwargs_non_sparse = kwargs.get('non_sparse', {})
+        assert isinstance(kwargs_sparse, dict) and isinstance(kwargs_non_sparse, dict)
         f = tables.open_file(path, mode='w')
         data_group = f.create_group("/", "data")
         data_sparse_group = f.create_group(data_group, "sparse")
-        self._store_sparse_m(self.sparse_m, f, data_sparse_group)
+        self._store_sparse_m(self.sparse_m, f, data_sparse_group,
+                             complevel=self.complevel, complib=self.complib, **kwargs_sparse)
         self._store_array_hdf(self.sparse.columns.values, f, data_sparse_group, "col_names")
-        self.non_sparse.to_hdf(path, key="data/non_sparse", complevel=9, complib='blosc')
+        self.non_sparse.to_hdf(path, key="data/non_sparse",
+                               complevel=self.complevel, complib=self.complib, **kwargs_non_sparse)
         f.close()
 
 
