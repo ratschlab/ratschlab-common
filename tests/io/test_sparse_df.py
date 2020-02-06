2ee0c395e26106c751c6eae009d1be7f9e4ff7a2
lorenzonoci <lore.noci@gmail.com>
2020-02-06 11:45:08 +0100

    test sparse df

---
+++ tests/io/test_sparse_df.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from pandas.api.types import is_sparse
+from pandas.testing import assert_frame_equal
+from ratschlab_common.io.sparse_df import SparseDFReader, SparseDFWriter
+
+def create_sparse_df():
+    # create sparse df
+    sparse_indexes = [0, 1]
+    df = pd.DataFrame(np.random.randn(10, 4))
+    random_index = np.random.randint(0, 2, size=(10,))
+    for i in sparse_indexes:
+        df[i][random_index == 0] = 0.0
+        df[i] = pd.SparseArray(df[0], fill_value=0.0)
+    assert is_sparse(df[0]) == True
+    return df
+
+def test_round_trip():
+    # create writer and save to disk
+    df_w = create_sparse_df()
+    path = "tests/data/myhdf5file.h5"
+    writer = SparseDFWriter(df_w)
+    writer.to_hdf(path)
+
+    # create reader and read df
+    reader = SparseDFReader(path)
+    df_r = reader.read_hdf()
+
+    assert_frame_equal(df_w, df_r)
+
+if __name__ == '__main__':
+    test_round_trip()
+
+
