fad134d340e7fb52ede93ae715d480162a0101f7
lorenzonoci <lore.noci@gmail.com>
2020-02-10 17:53:45 +0100

    fix signature and save all columns for right order

--- ratschlab_common/io/sparse_df.py
+++ ratschlab_common/io/sparse_df.py
@@ -17,14 +17,15 @@ def read_hdf(fpath):
     :param fpath: location of the hdf file
     :return: an instance of Pandas DataFrame
     '''
-    f = tables.open_file(fpath, mode='r')
-    sparse_group = f.get_node("/data/sparse")
-    sparse_m = _read_sparse_m_coo(sparse_group)
-    s_col_names = f.get_node("/data/sparse/col_names")
-    non_sparse = pd.read_hdf(fpath, key="/data/non_sparse")
-    sparse = pd.DataFrame.sparse.from_spmatrix(sparse_m, columns=s_col_names)
-    ret_df = pd.concat([sparse, non_sparse], axis=1)
-    f.close()
+    with tables.open_file(fpath, mode='r') as f:
+        sparse_group = f.get_node("/data/sparse")
+        sparse_m = _read_sparse_m_coo(sparse_group)
+        col_names = f.get_node("/col_names")
+        s_col_names = f.get_node("/data/sparse/sparse_col_names")
+        non_sparse = pd.read_hdf(fpath, key="/data/non_sparse")
+        sparse = pd.DataFrame.sparse.from_spmatrix(sparse_m, columns=s_col_names)
+        ret_df = pd.concat([sparse, non_sparse], axis=1)
+        ret_df = ret_df[col_names]
     return ret_df
 
 def _read_sparse_m(group):
@@ -72,43 +73,42 @@ def _store_non_sparse_df(df, path, complevel=5, complib='blosc', **kwargs):
                          complevel=complevel, complib=complib, **kwargs)
 
 
-def to_hdf(df, path, **kwargs):
+def to_hdf(df, path, sparse_hdf_filter_dict=None, non_sparse_hdf_filter_dict=None):
     '''
     write sparse dataframe to hdf file . The default compression level and complib are:
     complevel: 5
     complib: 'blosc'
     If you wish to change that, pass a dictionary to the function call as in the following example:
-    pars = {
-     'sparse': {
+    sparse_hdf_filter_dict = {
       'complevel': 6,
       'complib': 'zlib'
-     }
-     'non_sparse': {
+    }
+    non_sparse_hdf_filter_dict = {
       'complevel': 7,
       'complib': 'lzo'
-     }
     }
-    to_hdf(my_df, my_path, **pars).
+    to_hdf(my_df, my_path, sparse_hdf_filter_dict, non_sparse_hdf_filter_dict).
 
     The sparse matrix is saved in COO format
     :param df: an instance of Pandas DataFrame that you wish to save to file
     :param path: path of the
-    :param kwargs:
+    :param sparse_hdf_filter_dict: options for hdf filters (sparse part)
+    :param non_sparse_hdf_filter_dict: options for hdf filters (non sparse part)
     :return:
     '''
-    kwargs_sparse = kwargs.get('sparse', {})
-    kwargs_non_sparse = kwargs.get('non_sparse', {})
+    kwargs_sparse = sparse_hdf_filter_dict if sparse_hdf_filter_dict is not None else {}
+    kwargs_non_sparse = non_sparse_hdf_filter_dict if non_sparse_hdf_filter_dict is not None else {}
     assert isinstance(kwargs_sparse, dict) and isinstance(kwargs_non_sparse, dict)
 
     sparse_df, non_sparse_df = _split_sparse(df)
-    sparse_m = sparse.coo_matrix(sparse_df.values)
-    f = tables.open_file(path, mode='w')
-    data_group = f.create_group("/", "data")
-    data_sparse_group = f.create_group(data_group, "sparse")
-    _store_sparse_m_coo(sparse_m, f, data_sparse_group, **kwargs_sparse)
-    _store_array_hdf(sparse_df.columns.values, f, data_sparse_group, "col_names")
-    _store_non_sparse_df(non_sparse_df, path, **kwargs_non_sparse)
-    f.close()
+    sparse_m = sparse_df.sparse.to_coo()
+    with tables.open_file(path, mode='w') as f:
+        data_group = f.create_group("/", "data")
+        data_sparse_group = f.create_group(data_group, "sparse")
+        _store_array_hdf(df.columns.values, f, f.root, "col_names")
+        _store_sparse_m_coo(sparse_m, f, data_sparse_group, **kwargs_sparse)
+        _store_array_hdf(sparse_df.columns.values, f, data_sparse_group, "sparse_col_names")
+        _store_non_sparse_df(non_sparse_df, path, **kwargs_non_sparse)
 
 
 def _split_sparse(df):
--- tests/io/test_sparse_df.py
+++ tests/io/test_sparse_df.py
@@ -5,9 +5,8 @@ from pandas.testing import assert_frame_equal
 from ratschlab_common.io.sparse_df import read_hdf, to_hdf
 import random
 
-def create_sparse_df():
+def create_sparse_df(sparse_indexes):
     # create sparse df
-    sparse_indexes = [0, 1]
     random.seed(30)
     df = pd.DataFrame(np.random.randn(10, 4))
     random_index = np.random.randint(0, 2, size=(10,))
@@ -19,8 +18,8 @@ def create_sparse_df():
 
 def test_round_trip():
     # create writer and save to disk
-    df_w = create_sparse_df()
-    path = "tests/data/myhdf5file.h5"
+    df_w = create_sparse_df([0,1])
+    path = "/home/lorenzo/ETH/semester3/job/ratschlab-common/tests/data/myhdf5file.h5"
     to_hdf(df_w, path)
 
     # create reader and read df
@@ -28,7 +27,31 @@ def test_round_trip():
 
     assert_frame_equal(df_w, df_r)
 
+def test_column_order():
+    df_w = create_sparse_df([0,2])
+    path = "/home/lorenzo/ETH/semester3/job/ratschlab-common/tests/data/myhdf5file.h5"
+    to_hdf(df_w, path)
+
+    # create reader and read df
+    df_r = read_hdf(path)
+    assert list(df_w.columns.values) == list(df_r.columns.values)
+
+def test_options():
+    df_w = create_sparse_df([0, 2])
+    path = "/home/lorenzo/ETH/semester3/job/ratschlab-common/tests/data/myhdf5file.h5"
+    to_hdf(df_w, path,
+           sparse_hdf_filter_dict={'complevel': 6, 'complib': 'zlib'},
+           non_sparse_hdf_filter_dict={'complevel': 7,'complib': 'lzo'}
+           )
+
+    # create reader and read df
+    df_r = read_hdf(path)
+    assert_frame_equal(df_w, df_r)
+
+
 if __name__ == '__main__':
     test_round_trip()
+    test_column_order()
+    test_options()
 
 
