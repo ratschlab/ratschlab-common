1c3063c5307f2758011a58934a2b73b486691a7c
lorenzonoci <lore.noci@gmail.com>
2020-02-14 11:46:46 +0100

    test for pq-tool

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'console_scripts': [
             'bigmatrix_repack=ratschlab_common.scripts.bigmatrix_repack:main',
             'export_db_to_files=ratschlab_common.scripts.export_db_to_files:main',
-            'pq_tool=ratschlab_common.parquet_tools.pq_tool:pq_tool'
+            'pq-tool=ratschlab_common.parquet_tools.pq_tool:pq_tool'
         ],
     },
     install_requires=requirements,
